92636a2fc7e485d178d848d32701f99f813e938f
Muhammad Habibullah <muhammadhabibullah.id@gmail.com>
2022-07-30 09:13:30 +0700

    refactor: create product use case

--- usecase/admin/admin_create_product.go
+++ usecase/admin/admin_create_product.go
@@ -2,16 +2,11 @@ package admin
 
 import (
 	"context"
-	"fmt"
 
 	"jamtangan/domain"
 )
 
 func (u *adminUseCase) CreateProduct(ctx context.Context, product domain.Product) (domain.Product, error) {
-	if product.Price <= 0 {
-		return domain.Product{}, fmt.Errorf("price must be exist: %w", domain.ErrBadRequest)
-	}
-
 	err := u.productRepository.Create(ctx, &product)
 	if err != nil {
 		return domain.Product{}, err
